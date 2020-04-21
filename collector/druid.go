da28031db382f7cd84a909287f56b383b43a6065
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2020-04-21 20:05:19 +0530

    Updated code

--- collector/druid.go
+++ collector/druid.go
@@ -1,11 +1,38 @@
 package collector
 
 import (
-	"fmt"
 	"druid-exporter/utils"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
+// MetricCollector includes the list of metrics
+type MetricCollector struct {
+	DruidHealthStatus        *prometheus.Desc
+}
+
 // GetDruidMetrics returns the set of metrics for druid
-func GetDruidMetrics() {
-	fmt.Println(utils.GetDruidHealth("http://52.172.156.84:8081/status/health"))
+func GetDruidHealthMetrics() {
+	return utils.GetDruidHealth("http://52.172.156.84:8081/status/health")
+}
+
+// Describe will associate the value for druid exporter
+func (collector *MetricCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- collector.DruidHealthStatus
+}
+
+// Collector return the defined metrics
+func Collector() *MetricCollector{
+	return &MetricCollector{
+		DruidHealthStatus: prometheus.NewDesc("druid_health_status",
+			"Health of Druid, 1 is healthy 0 is not",
+			nil, prometheus.Labels{
+				"druid": "health",
+			},
+		),
+	}
+}
+
+// Collect will collect all the metrics
+func (collector *MetricCollector) Collect(ch chan<- prometheus.Metric) {
+	ch <- prometheus.MustNewConstMetric(collector.DruidHealthStatus, prometheus.CounterValue, GetDruidHealthMetrics())
 }
--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"net/http"
 	"druid-exporter/collector"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
-	collector.GetDruidMetrics()
+	data := collector.Collector()
+
+	prometheus.MustRegister(data)
+	http.Handle("/metrics", promhttp.Handler())
+	log.Info("Beginning to serve on port :8080")
+	http.ListenAndServe(":8080", nil)
 }
