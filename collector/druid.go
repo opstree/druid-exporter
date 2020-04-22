2ae03e0780f716351bbe8e88599bca42841bb55e
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2020-04-22 16:21:16 +0530

    Added tasks status

--- collector/druid.go
+++ collector/druid.go
@@ -13,6 +13,28 @@ type MetricCollector struct {
 	DataSourceCount          *prometheus.Desc
 	DruidTasks               *prometheus.Desc
 	DruidSupervisors         *prometheus.Desc
+	DruidSegmentCount        *prometheus.Desc
+}
+
+// SegementInterface is the interface for parsing segments data
+type SegementInterface []struct {
+	Name       string `json:"name"`
+	Properties struct {
+		Tiers struct {
+			DefaultTier struct {
+				Size           int64 `json:"size"`
+				ReplicatedSize int64 `json:"replicatedSize"`
+				SegmentCount   int   `json:"segmentCount"`
+			} `json:"_default_tier"`
+		} `json:"tiers"`
+		Segments struct {
+			MaxTime        time.Time `json:"maxTime"`
+			Size           int64     `json:"size"`
+			MinTime        time.Time `json:"minTime"`
+			Count          int       `json:"count"`
+			ReplicatedSize int64     `json:"replicatedSize"`
+		} `json:"segments"`
+	} `json:"properties"`
 }
 
 // GetDruidMetrics returns the set of metrics for druid
@@ -21,10 +43,10 @@ func GetDruidHealthMetrics() float64 {
 }
 
 // GetDruidDatasource returns the datasources of druid
-func GetDruidDatasource() []string{
-	respData, _ := utils.GetDruidResponse("http://52.172.156.84:8081/druid/coordinator/v1/metadata/datasources")
+func GetDruidSegmentData() []string{
+	respData, _ := utils.GetDruidResponse("http://52.172.156.84:8081/druid/coordinator/v1/datasources?simple")
 
-	var metric []string
+	var metric SegementInterface
 	json.Unmarshal(respData, &metric)
 	return metric
 }
@@ -52,6 +74,7 @@ func (collector *MetricCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- collector.DataSourceCount
 	ch <- collector.DruidTasks
 	ch <- collector.DruidSupervisors
+	ch <- collector.DruidSegmentCount
 }
 
 // Collector return the defined metrics
@@ -73,7 +96,11 @@ func Collector() *MetricCollector{
 		),
 		DruidSupervisors: prometheus.NewDesc("druid_supervisors",
 			"Druid supervisors status",
-			[]string{"datasource", "healthy", "state"}, nil,
+			[]string{"supervisor_name", "healthy", "state"}, nil,
+		),
+		DruidSegmentCount: prometheus.NewDesc("druid_segement_count",
+			"Druid segment count",
+			[]string{"name"}, nil,
 		),
 	}
 }
@@ -81,13 +108,16 @@ func Collector() *MetricCollector{
 // Collect will collect all the metrics
 func (collector *MetricCollector) Collect(ch chan<- prometheus.Metric) {
 	ch <- prometheus.MustNewConstMetric(collector.DruidHealthStatus, prometheus.CounterValue, GetDruidHealthMetrics())
-	for _, data := range GetDruidDatasource() {
-		ch <- prometheus.MustNewConstMetric(collector.DataSourceCount, prometheus.GaugeValue, float64(1), data)
+	for _, data := range SegementInterface() {
+		ch <- prometheus.MustNewConstMetric(collector.DataSourceCount, prometheus.GaugeValue, float64(1), data.Name)
 	}
 	for _, data := range GetDruidTasks() {
 		ch <- prometheus.MustNewConstMetric(collector.DruidTasks, prometheus.GaugeValue, float64(1), fmt.Sprintf("%v",data["dataSource"]), fmt.Sprintf("%v", data["groupId"]), fmt.Sprintf("%v", data["status"]), fmt.Sprintf("%v", data["createdTime"]))
 	}
 	for _, data := range GetDruidSupervisors() {
 		ch <- prometheus.MustNewConstMetric(collector.DruidSupervisors, prometheus.GaugeValue, float64(1), fmt.Sprintf("%v",data["id"]), fmt.Sprintf("%v", data["healthy"]), fmt.Sprintf("%v", data["detailedState"]))
+	}
+	for _, data := range SegementInterface() {
+		ch <- prometheus.MustNewConstMetric(collector.DruidSegmentCount, prometheus.GaugeValue, float64(data.Properties.Segments.Count), data.Name)
 	}
 }
