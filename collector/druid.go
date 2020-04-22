a992778b9599c510fc8d48ebc26a5644d7ffd6a9
iamabhishek-dubey <abhishekbhardwaj510@gmail.com>
2020-04-22 14:52:04 +0530

    Added tasks status

--- collector/druid.go
+++ collector/druid.go
@@ -70,6 +70,6 @@ func (collector *MetricCollector) Collect(ch chan<- prometheus.Metric) {
 		ch <- prometheus.MustNewConstMetric(collector.DataSourceCount, prometheus.GaugeValue, float64(1), data)
 	}
 	for _, data := range GetDruidTasks() {
-		ch <- prometheus.MustNewConstMetric(collector.DruidTasks, prometheus.GaugeValue, float64(1), fmt.Sprintf("%v",data["dataSource"]), fmt.Sprintf(data["groupId"]), fmt.Sprintf(data["status"]))
+		ch <- prometheus.MustNewConstMetric(collector.DruidTasks, prometheus.GaugeValue, float64(1), fmt.Sprintf("%v",data["dataSource"]), fmt.Sprintf("%v", data["groupId"]), fmt.Sprintf("%v", data["status"]))
 	}
 }
