4a57f8bbdaee41bfa1388c898b3bb0259021d80f
Herb <i@4leaf.me>
2020-09-25 10:09:03 +0800

    cleanup too old druid emited metrics
    Signed-off-by: Herb <i@4leaf.me>

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/gorilla/mux v1.7.4
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/prometheus/client_golang v1.5.1
+	github.com/robfig/cron/v3 v3.0.1
 	github.com/rs/zerolog v1.18.0
 	github.com/sirupsen/logrus v1.4.2
 	github.com/zsais/go-gin-prometheus v0.1.0
--- listener/druid_endpoint.go
+++ listener/druid_endpoint.go
@@ -17,6 +17,7 @@ import (
 
 // DruidHTTPEndpoint is the endpoint to listen all druid metrics
 func DruidHTTPEndpoint(histogram *prometheus.HistogramVec, gauge *prometheus.GaugeVec, dnsCache *cache.Cache) http.HandlerFunc {
+	gaugeCleaner := newCleaner(gauge, 10)
 	return func(w http.ResponseWriter, req *http.Request) {
 		var druidData []map[string]interface{}
 		reqHeader, _ := header.ParseValueAndParams(req.Header, "Content-Type")
@@ -66,12 +67,14 @@ func DruidHTTPEndpoint(histogram *prometheus.HistogramVec, gauge *prometheus.Gau
 								"host":        host,
 							}).Observe(value)
 
-							gauge.With(prometheus.Labels{
+							labels := prometheus.Labels{
 								"metric_name": strings.Replace(metric, "/", "-", 3),
 								"service":     strings.Replace(service, "/", "-", 3),
 								"datasource":  entryDatasource.(string),
 								"host":        host,
-							}).Set(value)
+							}
+							gaugeCleaner.add(labels)
+							gauge.With(labels).Set(value)
 						}
 					} else { // Single datasource
 						histogram.With(prometheus.Labels{
@@ -81,12 +84,14 @@ func DruidHTTPEndpoint(histogram *prometheus.HistogramVec, gauge *prometheus.Gau
 							"host":        host,
 						}).Observe(value)
 
-						gauge.With(prometheus.Labels{
+						labels := prometheus.Labels{
 							"metric_name": strings.Replace(metric, "/", "-", 3),
 							"service":     strings.Replace(service, "/", "-", 3),
 							"datasource":  datasource.(string),
 							"host":        host,
-						}).Set(value)
+						}
+						gaugeCleaner.add(labels)
+						gauge.With(labels).Set(value)
 					}
 				} else { // Missing datasource case
 					histogram.With(prometheus.Labels{
@@ -96,12 +101,14 @@ func DruidHTTPEndpoint(histogram *prometheus.HistogramVec, gauge *prometheus.Gau
 						"host":        host,
 					}).Observe(value)
 
-					gauge.With(prometheus.Labels{
+					labels := prometheus.Labels{
 						"metric_name": strings.Replace(metric, "/", "-", 3),
 						"service":     strings.Replace(service, "/", "-", 3),
 						"datasource":  "",
 						"host":        host,
-					}).Set(value)
+					}
+					gaugeCleaner.add(labels)
+					gauge.With(labels).Set(value)
 				}
 			}
 
